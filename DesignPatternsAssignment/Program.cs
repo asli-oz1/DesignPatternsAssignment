47037092b1c0b050409694beff078e566901f344
asli-oz1 <f221229012@ktun.edu.tr>
2026-05-20 22:26:59 +0300

    feat: create initial cart system with naive design

---
+++ DesignPatternsAssignment/Program.cs
@@ -0,0 +1,7 @@
+﻿using DesignPatternsAssignment.src;
+
+Cart sepet = new Cart();
+
+double sonuc = sepet.IndirimHesapla(1000, "ogrenci");
+
+Console.WriteLine("İndirimli fiyat: " + sonuc);
---
+++ DesignPatternsAssignment/src/Cart.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternsAssignment.src
+{
+    public class Cart
+    {
+        public double IndirimHesapla(double toplamFiyat, string indirimTuru)
+        {
+            double indirimMiktari = 0;
+
+            
+            if (indirimTuru == "ogrenci")
+            {
+                indirimMiktari = toplamFiyat * 10 / 100;
+            }
+
+            
+            else if (indirimTuru == "yilbasi")
+            {
+                indirimMiktari = toplamFiyat * 20 / 100;
+            }
+
+            
+            else if (indirimTuru == "premium")
+            {
+                indirimMiktari = toplamFiyat * 15 / 100;
+            }
+
+            return toplamFiyat - indirimMiktari;
+        }
+    }
+}
